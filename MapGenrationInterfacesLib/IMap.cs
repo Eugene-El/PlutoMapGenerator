3abf1c8a42d878f5d9ec43b3455786f8f84ac321
Eugene El <Eugene@DESKTOP-TAUQH74>
2019-05-07 20:44:35 +0300

    Add map generation interface library

---
+++ MapGenrationInterfacesLib/IMap.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MapGenrationInterfacesLib
+{
+    public interface IMap
+    {
+        ITitle Get(int x, int y);
+        void Set(int x, int y, ITitle title);
+    }
+}
